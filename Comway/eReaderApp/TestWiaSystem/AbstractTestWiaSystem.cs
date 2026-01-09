d7cc04645506882c350d5046f1f500f8aa091d5d
mutoyuna-comway <muto-yuna@com-way.co.jp>
2026-01-09 15:40:08 +0900

    TestWiaSystemを追加

---
+++ Comway/eReaderApp/TestWiaSystem/AbstractTestWiaSystem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Wia.Abstractions;
+
+namespace TestWiaSystem
+{
+    [TestClass]
+    public abstract class AbstractTestWiaSystem
+    {
+        public abstract IWiaSystem WiaSystem { get; set; }
+
+        [TestMethod]
+        public void 基本テストパターン1()
+        {
+            //Assert.AreEqual("XXXXX/YYYYY" ,WiaSystem.GetJobFolder());
+        }
+    }
+}
---
+++ Comway/eReaderApp/TestWiaSystem/TestStubWiaSystem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Wia.Abstractions;
+
+namespace TestWiaSystem
+{
+
+    [TestClass]
+    public class TestStubWiaSystem : AbstractTestWiaSystem
+    {
+        public override IWiaSystem WiaSystem { get; set; }// = "StubWiaSystem"
+
+        [TestMethod]
+        public void 追加テストパターン1()
+        {
+            //Assert.AreEqual("XXXXX/YYYYY" ,WiaSystem.GetJobFolder());
+        }
+
+
+    }
+}
