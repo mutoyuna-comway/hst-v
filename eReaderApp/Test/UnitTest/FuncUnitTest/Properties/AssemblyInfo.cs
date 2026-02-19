51f09bb850e468cb6138fa47c3a7233dd4de45e1
katokasumi1125-HSTv <kato.kasumi1125@gmail.com>
2026-02-19 11:19:51 +0900

    機能ユニットテストプロジェクトを新規追加

---
+++ eReaderApp/Test/UnitTest/FuncUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FuncUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FuncUnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f72e3f7-4741-4154-94a2-a168e16e7610")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
